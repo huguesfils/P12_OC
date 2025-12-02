de5020f66a2c074d16af7a46fc6d21cb8e910d0f
hugues <hugues.fils@beeldi.com>
2025-12-02 12:17:48 +0100

    Initial commit

---
+++ Joiefull/Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Hugues Fils Caparos on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Joiefull/JoiefullTests/JoiefullTests.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullTests.swift
+//  JoiefullTests
+//
+//  Created by Hugues Fils Caparos on 02/12/2025.
+//
+
+import Testing
+@testable import Joiefull
+
+struct JoiefullTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
