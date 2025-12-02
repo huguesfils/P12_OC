c59643b572a786d6f4266c0f6ea5ad1d29ee5a95
hugues <hugues.fils@beeldi.com>
2025-12-02 16:55:42 +0100

    Feat: Folders + ItemCardView init

---
+++ Joiefull/Joiefull/Components/ItemCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCardView.swift
+//  Joiefull
+//
+//  Created by Hugues Fils Caparos on 02/12/2025.
+//
+
+import SwiftUI
+
+struct ItemCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemCardView()
+}
